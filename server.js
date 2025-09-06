34661159799dce93381123ce374dbdb4c51b699c
m12458744 <m21352@otpbale.freehost.io>
2025-09-06 13:15:51 +0330

    Update server.js

--- server.js
+++ server.js
@@ -1,39 +1,18 @@
-require('dotenv').config();
-const { Telegraf } = require('telegraf');
-const express = require('express');
+bot.on(['photo', 'video', 'document'], async (ctx) => {
+  let fileId;
 
-const bot = new Telegraf(process.env.BOT_TOKEN);
-const app = express();
+  if (ctx.message.photo) {
+    fileId = ctx.message.photo[ctx.message.photo.length - 1].file_id;
+  } else if (ctx.message.video) {
+    fileId = ctx.message.video.file_id;
+  } else if (ctx.message.document) {
+    fileId = ctx.message.document.file_id;
+  }
 
-bot.start((ctx) => ctx.reply('سلام! ربات آماده است.'));
-bot.help((ctx) => ctx.reply('برای استفاده از ربات، فایل بفرستید تا لینک دریافت کنید.'));
-
-bot.on('message', async (ctx) => {
-  try {
-    if (ctx.message.document) {
-      const fileId = ctx.message.document.file_id;
-      const link = await ctx.telegram.getFileLink(fileId);
-      ctx.reply(`لینک فایل شما:\n${link.href}`);
-    } else {
-      ctx.reply('لطفا فایل ارسال کنید.');
-    }
-  } catch (err) {
-    console.error(err);
-    ctx.reply('خطا پیش آمده، لطفا دوباره تلاش کنید.');
+  if (fileId) {
+    const link = await ctx.telegram.getFileLink(fileId);
+    await ctx.reply(`لینک دانلود فایل شما:\n${link.href}`);
+  } else {
+    await ctx.reply('لطفا فایل ارسال کنید.');
   }
 });
-
-const PORT = process.env.PORT || 10000;
-
-app.get('/', (req, res) => {
-  res.send('ربات فعال است');
-});
-
-app.listen(PORT, () => {
-  console.log(`سرور روی پورت ${PORT} در حال اجراست.`);
-});
-
-bot.launch();
-
-process.once('SIGINT', () => bot.stop('SIGINT'));
-process.once('SIGTERM', () => bot.stop('SIGTERM'));
