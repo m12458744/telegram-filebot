7c504c80d35691ba897c830dc9f53ecbef6fcb0d
m12458744 <m21352@otpbale.freehost.io>
2025-09-08 08:42:56 +0330

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const bot = require('./bot');
 
 bot.launch()
-  .then(() => console.log('ربات راه‌اندازی شد!'))
+  .then(() => console.log('🤖 ربات با موفقیت راه‌اندازی شد'))
   .catch(console.error);
 
+// مدیریت درست خاموش شدن ربات
 process.once('SIGINT', () => bot.stop('SIGINT'));
 process.once('SIGTERM', () => bot.stop('SIGTERM'));
